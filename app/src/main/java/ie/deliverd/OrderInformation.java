c0eae14549b21f87ef1927ec7aa661cf7e290d4b
Eoghan Kenny <x16774931@student.ncirl.ie>
2018-02-26 11:45:55 +0000

    added order details activity

---
+++ app/src/main/java/ie/deliverd/OrderInformation.java
@@ -0,0 +1,13 @@
+package ie.deliverd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_information);
+    }
+}
